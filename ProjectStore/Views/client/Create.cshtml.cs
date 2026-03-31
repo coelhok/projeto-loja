e69a4d2464148af762f4ce00e69b151ea6fa7c1b
kevin alencar <kevinalencar169@gmail.com>
2026-03-31 17:51:59 -0300

    new tab create from client and bug fix

---
+++ ProjectStore/Views/client/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectStore.Views.client
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
